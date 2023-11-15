c4643736c98cbdd5e8d7108a7f1a1c3169dd8984
marcinoles87 <marcinoles87@gmail.com>
2023-11-15 14:36:33 +0100

    create fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  // fetch("https://randomuser.me/api/?results=2")
+  // .then( (result) => {
+  //   return result.json()
+  // } )
+
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?results=2`)
+     .then((response) => { return response.json()})
+     .then( (data) => {
+      
+     })
+   }, []);
+  
+  
   return (
     <div className="App">
       <h1>Exercise</h1>
+
+
     </div>
   );
 }
