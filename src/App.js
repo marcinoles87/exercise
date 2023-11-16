47b60ad45bc8885e4565eff7a71dfba9b5828a86
marcinoles87 <marcinoles87@gmail.com>
2023-11-16 14:46:39 +0100

    create fetch data

--- src/App.js
+++ src/App.js
@@ -9,21 +9,33 @@ function App() {
   //   return result.json()
   // } )
 
-  const [data, setData] = useState()
+  const [data, setData] = useState(null)
+
+
 
   useEffect(() => {
     fetch(`https://randomuser.me/api/?results=2`)
      .then((response) => { return response.json()})
      .then( (data) => {
+        setData(data.results)
+        console.log(data.results)
       
      })
    }, []);
   
-  
+  console.log(data)
   return (
     <div className="App">
       <h1>Exercise</h1>
-
+      {data && data.map( ({gender , email , picture} , index) => {
+        return(
+          <>
+          <p key={index}> {gender} email : {email}</p>
+         
+          <img src={picture.large} alt={index}></img>
+          </>
+        )
+      })}
 
     </div>
   );
